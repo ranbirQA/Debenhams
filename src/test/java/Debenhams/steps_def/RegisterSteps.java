128a0fc9def2f5d8a05e70a459abcc62b20735d1
ranbirQA <aranbir50@gmail.com>
2019-05-04 01:11:12 +0100

    intial commit test new

---
+++ src/test/java/Debenhams/steps_def/RegisterSteps.java
@@ -0,0 +1,35 @@
+package Debenhams.steps_def;
+
+import cucumber.api.DataTable;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class RegisterSteps {
+    @Given("^i am on homepage$")
+    public void i_am_on_homepage()  {
+
+    }
+
+    @When("^i navigate to \"([^\"]*)\"$")
+    public void i_navigate_to(String arg1)  {
+
+    }
+
+    @When("^I enter user email address$")
+    public void i_enter_user_email_address()  {
+
+    }
+
+    @When("^i fill the user detail$")
+    public void i_fill_the_user_detail(DataTable arg1)  {
+
+    }
+
+    @Then("^i should see a empty field message \"([^\"]*)\"$")
+    public void i_should_see_a_empty_field_message(String arg1)  {
+
+    }
+
+
+}
